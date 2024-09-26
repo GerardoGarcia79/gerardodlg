918222f1e7dfd5d85dc68f9c4a9296181685121f
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-09-26 09:06:23 -0600

    Build and style StackTools component.

--- src/components/StackSection.tsx
+++ src/components/StackSection.tsx
@@ -1,4 +1,5 @@
 import StackFrontend from "./StackFrontend"
+import StackTools from "./StackTools"
 
 
 
@@ -6,6 +7,7 @@ const StackSection = () => {
   return (
     <div className="mt-5 grid grid-cols-1 gap-3 md:gap-5 md:grid-cols-2 lg:grid-cols-3">
         <StackFrontend />
+        <StackTools />
     </div>
   )
 }
