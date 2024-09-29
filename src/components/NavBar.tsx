b3858b56df3f07fca80a853c8e9cf2eceae59715
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-09-28 19:41:54 -0600

    Add border animation in HireMeButtonComponent.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <>
     <FontAwesomeIcon icon={faBars} className="w-9 h-9 md:hidden fixed top-2 right-2 text-white z-10" />
-    <div className={`${ isScrolled ? 'bg-[#414141]' : ''} rounded-full fixed w-fit text-center top-1 left-1/2 -translate-x-1/2 z-10`}>
+    <div className={`${ isScrolled ? 'bg-[#414141]' : ''} rounded-full fixed w-fit text-center top-4 left-1/2 -translate-x-1/2 z-10`}>
         <ul className="flex justify-center text-white text-base md:[&_*]:p-2">
             <li className="hidden md:list-item">
               <a href="#header">Home</a>
