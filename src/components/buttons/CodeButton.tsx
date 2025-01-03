146c3d21faed4539ae9b22e294e15b8c82490e8c
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-03 13:55:11 -0600

    Change code and demo buttons bg color

--- src/components/buttons/CodeButton.tsx
+++ src/components/buttons/CodeButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 const CodeButton = ({ code }: Props) => {
   return (
     <a href={code} target="_blank" className="text-lg">
-      <button className="bg-[#bfbfbf] dark:bg-[#303030] p-2 rounded-xl hover:bg-[#0081b4] dark:hover:bg-[#0081b4] transition ease-out hover:scale-110 duration-300 flex justify-center items-center border-[1px] border-white/20 hover:border-2 hover:border-white">
+      <button className="bg-[#f2f2f2] dark:bg-[#303030] p-2 rounded-xl hover:bg-[#0081b4] dark:hover:bg-[#0081b4] transition ease-out hover:scale-110 duration-300 flex justify-center items-center border-[1px] border-white/20 hover:border-2 hover:border-white">
         <FiGithub size="25px" />
         <span>Code</span>
       </button>
